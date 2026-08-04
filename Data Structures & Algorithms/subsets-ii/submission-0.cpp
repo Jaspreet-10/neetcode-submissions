4c2560a2f082d8b1f044cac45eb8757e32da22e6
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2026-08-03 17:17:32 -0700

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void getSubsets(int index, vector<int>&nums, int n, vector<int>&temp, map<vector<int>, int>&m, vector<vector<int>>&ans){
+        if(index>=n){
+            // sort(temp.begin(), temp.end());
+            // if(m[temp] == 0){
+            ans.push_back(temp);
+            // m[temp]++;
+            // }
+            return;
+        }
+        temp.push_back(nums[index]);
+        getSubsets(index+1, nums, n, temp, m, ans);
+        temp.pop_back();
+        while (index != nums.size() - 1 && nums[index] == nums[index + 1])
+            index++;
+        getSubsets(index+1, nums, n, temp, m, ans);
+        return;
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        map<vector<int>, int>m;
+        sort(nums.begin(), nums.end());
+        getSubsets(0, nums, nums.size(), temp, m, ans);
+        return ans;
+    }
+};
