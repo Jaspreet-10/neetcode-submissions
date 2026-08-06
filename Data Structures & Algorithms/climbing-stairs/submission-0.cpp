05880474c406b0eb6ef7a3f81a01840fe2f33fe6
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2026-08-06 16:58:54 -0700

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countWays(int index, int n, vector<int>&dp){
+        if(index == n) return 1;
+        if(index>n) return -1;
+        if(dp[index]!=0) return dp[index];
+        int oneStep = max(0, countWays(index+1, n, dp));
+        int twoSteps = max(0, countWays(index+2, n, dp));
+        dp[index] = oneStep+twoSteps;
+        return dp[index];
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n, 0);
+        return countWays(0, n, dp);
+    }
+};
