cbdce8586775f25e807c839026302716578ce5bc
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2026-08-26 15:02:43 -0700

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = prices[0], maxProfit = 0;
+        for(int i = 0 ; i < prices.size() ; ++i){
+            if(prices[i]>buy){
+                maxProfit = max(maxProfit, prices[i]-buy);
+            }
+            if(prices[i]<buy) buy = prices[i];
+        }
+        return maxProfit;
+    }
+};
