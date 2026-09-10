8cadc78cb6661077108a6e496f629762ecb56e43
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2026-09-09 18:37:54 -0700

    Add: time-based-key-value-store - submission-11

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-11.cpp
@@ -0,0 +1,25 @@
+class TimeMap {
+public:
+    unordered_map<string, vector<pair<int, string>>>m;
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        m[key].push_back({timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+        int low = 0, high = m[key].size()-1;
+        int ans = -1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(m[key][mid].first <= timestamp){
+                ans = mid;
+                low = mid+1;
+            }
+            else high = mid-1;
+        }
+        return ans == -1 ? "" : m[key][ans].second;
+    }
+};
