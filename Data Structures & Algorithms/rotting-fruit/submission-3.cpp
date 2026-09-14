7e40c0a090446c42fba05b73ba690e7bd593b918
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2026-09-13 18:07:45 -0700

    Add: rotting-fruit - submission-3

---
+++ Data Structures & Algorithms/rotting-fruit/submission-3.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        queue<pair<pair<int,int>, int>>q;
+        int m = grid.size(), n = grid[0].size(), count = 0;
+        for(int i = 0 ; i < m ; ++i){
+            for(int j = 0 ; j < n ; ++j){
+                if(grid[i][j] == 2) q.push({{i, j}, 0});
+            }
+        }
+        while(!q.empty()){
+            int i = q.front().first.first;
+            int j = q.front().first.second;
+            count = q.front().second;
+            q.pop();
+            int r[4] = {0, 1, 0, -1};
+            int c[4] = {1, 0, -1, 0};
+            for(int k = 0 ; k < 4 ; ++k){
+                int rc = r[k] + i;
+                int cc = c[k] + j;
+                if(rc>=0 and rc<m and cc>=0 and cc<n and grid[rc][cc] == 1){
+                    grid[rc][cc] = -1;
+                    q.push({{rc, cc}, count+1});
+                }
+            }
+        }
+         for(int i = 0 ; i < m ; ++i){
+            for(int j = 0 ; j < n ; ++j){
+                if(grid[i][j] == 1) return -1;
+            }
+        }
+        return count;
+    }
+};
