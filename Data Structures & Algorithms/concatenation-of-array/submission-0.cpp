6ccd4337352e29781380a40f96f970c48db1df1b
Jaspreet Mehra <64219292+Jaspreet-10@users.noreply.github.com>
2026-09-21 20:24:23 -0700

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int i = 0, size = nums.size();
+        while(i<size){
+            nums.push_back(nums[i]);
+            ++i;
+        }
+        return nums;
+    }
+};
